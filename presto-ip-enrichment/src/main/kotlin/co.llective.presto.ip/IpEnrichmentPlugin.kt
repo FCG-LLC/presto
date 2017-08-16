6fedfb9139b72e72466fe03b0e6e51ae0e492aa2
IC3Q <dmnkluba@gmail.com>
2017-08-16 14:12:45 +0200

    Added dummy function

--- presto-ip-enrichment/src/main/kotlin/co.llective.presto.ip/IpEnrichmentPlugin.kt
+++ presto-ip-enrichment/src/main/kotlin/co.llective.presto.ip/IpEnrichmentPlugin.kt
@@ -1,11 +1,45 @@
 package co.llective.presto.ip
 
 import com.facebook.presto.spi.Plugin
-
-import java.util.Collections.emptySet
+import com.facebook.presto.spi.function.*
+import com.facebook.presto.spi.type.StandardTypes
+import com.google.common.collect.ImmutableSet
+import io.airlift.slice.Slice
+import io.airlift.slice.Slices
 
 class IpEnrichmentPlugin : Plugin {
     override fun getFunctions(): Set<Class<*>> {
-        return emptySet<Class<*>>()
+        return ImmutableSet.of(DummyFunction.javaClass)//::class.java)
+    }
+}
+
+class DummyFunction {
+    companion object {
+        @TypeParameter("T")
+        @ScalarFunction("DUMMY")
+        @Description("Returns dummy string")
+        @SqlType(StandardTypes.VARCHAR)
+//        @JvmStatic
+        fun goForDummySlice(@SqlNullable @SqlType("T") value: Slice): Slice {
+            return Slices.utf8Slice("dummy")
+        }
+
+        @TypeParameter("T")
+        @ScalarFunction("DUMMY")
+        @Description("Returns dummy string")
+        @SqlType(StandardTypes.VARCHAR)
+//        @JvmStatic
+        fun goForDummyInt(@SqlNullable @SqlType("T") value: Integer): Slice {
+            return Slices.utf8Slice("dummy")
+        }
+
+        @TypeParameter("T")
+        @ScalarFunction("DUMMY")
+        @Description("Returns dummy string")
+        @SqlType(StandardTypes.VARCHAR)
+//        @JvmStatic
+        fun goForDummyLong(@SqlNullable @SqlType("T") value: Long): Slice {
+            return Slices.utf8Slice("dummy")
+        }
     }
 }
