f966049961aeede76713db445228e0906997d9ce
IC3Q <dmnkluba@gmail.com>
2017-08-23 12:15:21 +0200

    Changed INT parameter to java.lang.Long

--- presto-ip-enrichment/src/main/kotlin/co.llective.presto.ip/ApplicationNameFunction.kt
+++ presto-ip-enrichment/src/main/kotlin/co.llective.presto.ip/ApplicationNameFunction.kt
@@ -1,6 +1,5 @@
 package co.llective.presto.ip
 
-import com.facebook.presto.spi.function.Description
 import com.facebook.presto.spi.function.ScalarFunction
 import com.facebook.presto.spi.function.SqlNullable
 import com.facebook.presto.spi.function.SqlType
@@ -8,20 +7,20 @@ import com.facebook.presto.spi.type.StandardTypes
 import io.airlift.slice.Slice
 import io.airlift.slice.Slices
 
-@ScalarFunction("application_name")
-@Description("Returns application name")
 object ApplicationNameFunction {
 
+    @ScalarFunction("application_name")
     @SqlType(StandardTypes.VARCHAR)
     @JvmStatic
-    fun applicationNameWithPort(
+    fun applicationName(
             @SqlNullable @SqlType(StandardTypes.BIGINT) ip1: java.lang.Long,
             @SqlNullable @SqlType(StandardTypes.BIGINT) ip2: java.lang.Long,
-            @SqlNullable @SqlType(StandardTypes.INTEGER) port: java.lang.Integer
+            @SqlNullable @SqlType(StandardTypes.INTEGER) port: java.lang.Long
     ): Slice {
         return Slices.utf8Slice("app_name_with_port")
     }
 
+    @ScalarFunction("application_name")
     @SqlType(StandardTypes.VARCHAR)
     @JvmStatic
     fun applicationName(
