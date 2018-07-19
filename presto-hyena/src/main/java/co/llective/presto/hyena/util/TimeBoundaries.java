4b156c63e3cebaf773ea86ad815362d895a1b202
IC3Q <dmnkluba@gmail.com>
2018-07-19 15:33:10 +0200

    Added handling of one-side ts filter

---
+++ presto-hyena/src/main/java/co/llective/presto/hyena/util/TimeBoundaries.java
@@ -0,0 +1,34 @@
+package co.llective.presto.hyena.util;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TimeBoundaries
+{
+    private final Long start;
+    private final Long end;
+
+    @JsonCreator
+    public TimeBoundaries(@JsonProperty("start") Long start, @JsonProperty("end") Long end)
+    {
+        this.start = start;
+        this.end = end;
+    }
+
+    public static TimeBoundaries of(Long start, Long end)
+    {
+        return new TimeBoundaries(start, end);
+    }
+
+    @JsonProperty
+    public Long getStart()
+    {
+        return start;
+    }
+
+    @JsonProperty
+    public Long getEnd()
+    {
+        return end;
+    }
+}
